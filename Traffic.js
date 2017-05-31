91bdd70863cd1eecc808893d4fe5379c85c8c970
kNastaran7 <mrs.eglantine@gmail.com>
2017-05-31 13:00:15 +0000

    Update and rename Traffic.js to Init.js

--- Traffic.js
+++
@@ -1,8 +0,0 @@
-var fruits = ["Banana", "Orange", "Apple", "Mango"];
-document.getElementById("demo").innerHTML = fruits;
-
-function myFunction() {
-    fruits.sort();
-    fruits.reverse();
-    document.getElementById("demo").innerHTML = fruits;
-}
