129a5f6f613da78d5d9612fd14f5bff7d27e27d3
kNastaran7 <mrs.eglantine@gmail.com>
2017-05-31 10:13:42 +0430

    Create Traffic.js

---
+++ Traffic.js
@@ -0,0 +1,10 @@
+<script>
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits;
+
+function myFunction() {
+    fruits.sort();
+    fruits.reverse();
+    document.getElementById("demo").innerHTML = fruits;
+}
+</script>
